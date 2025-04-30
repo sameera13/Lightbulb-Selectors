e16047951d1bf4200bc03eaa9bec5be94550359d
SameeraChinta13 <sameera.chinta@verizon.com>
2025-04-30 14:49:24 -0500

    Added JS

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Write your code here
+const bulbs = document.querySelectorAll('.lightbulb');
+const subtitle = document.querySelector('.subtitle');
+let clickCount = 0;
+bulbs.forEach(bulb => {
+  bulb.addEventListener('click', () => {
+    bulb.classList.toggle('active');
+    clickCount++;
+    subtitle.textContent = `You've clicked the lights ${clickCount} times`;
+  });
+});
